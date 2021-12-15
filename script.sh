afdb842e100d495e0160d3ebc8acc865b7d6ed98
mytutes-github <prahuram.tutes@gmail.com>
2021-12-15 21:54:11 +0530

    Testing web-hook

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
+# Tomcat Dependencies #
+mv /usr/local/tomcat/webapps /usr/local/tomcat/webapps2
+mv /usr/local/tomcat/webapps.dist/ /usr/local/tomcat/webapps
+mv /usr/local/tomcat/webapps2/* /usr/local/tomcat/webapps
 mv /usr/local/tomcat/webapps /usr/local/tomcat/webapps2
 mv /usr/local/tomcat/webapps.dist/ /usr/local/tomcat/webapps
 mv /usr/local/tomcat/webapps2/* /usr/local/tomcat/webapps
